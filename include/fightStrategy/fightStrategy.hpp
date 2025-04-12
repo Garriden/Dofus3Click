a93084cca9b197a8e3f3a8c2c789d821a8c6ce0e
teteDiglett <Garridocho@gmail.com>
2025-04-12 17:50:38 +0200

    Fight strategies on herency.

---
+++ include/fightStrategy/fightStrategy.hpp
@@ -0,0 +1,55 @@
+#pragma once
+#include <vector>
+
+class FightStrategy {
+    public:
+        FightStrategy();
+        //FightStrategy();
+        virtual ~FightStrategy() = default;
+
+        virtual int Execute() = 0;
+        virtual void PrintStrategyName() = 0;
+
+        
+    protected:
+
+
+        int _turn;
+        int _myXPositionInMenuFight;
+        std::vector<int> _enemiesXPositionInMenuFight;
+
+        enum FightPreparationState {
+            FIGHT_SET                   = 0,
+            CHANGE_SPELLS_MENU          = 1,
+            FIND_MY_POSITION_MENU       = 2,
+            FIND_ENEMY_POSITIONS_MENU   = 3,
+            READY_TO_FIGHT              = 4,
+            START_FIGHT_STRATEGY        = 5,
+            CHECK_FIGHT_ENDED           = 6,
+            AFTER_FIGHT_SET             = 7,
+            AFTER_FIGHT_SIT             = 8,
+            WAIT_UNTIL_HEALED           = 9,
+            RETURN_E_OK                 = 10
+        };
+
+        
+        void DefendMyself();
+
+        /**
+         * spell: from menuMappings enums.
+         * who: to myself or enemies.
+         * upperRow: True if spell on the upper row(SpellsRow::SPELLS_ROW), fals if spell on under row(SpellsCtrlRow::SPELLS_CTRL_ROW).
+         */
+        void ThrowSpell(int spell, int who, int upperRow);
+        void ThrowSpellToEnemies(int spell, int upperRow);
+        void ThrowSpellToMyself(int spell, int upperRow);
+
+        // Click "moves" times in a random pixel around the center of the screen. To the Pj to move before ending the turn.
+        void RandomMovePj(int moves);
+
+        void PassTurn();
+
+        void StartFight();
+        void WaitForMyTurn();
+        int AfterFight();
+};
---
+++ src/fightStrategy/professionMobStrategy.cpp
@@ -0,0 +1,63 @@
+#include "fightStrategy/professionMobStrategy.hpp"
+#include "menuMappings.hpp"
+#include "basicOperations.hpp"
+#include "checks.hpp"
+#include "zaap.hpp"
+#include "system/inputs.hpp"
+#include "system/file.hpp"
+#include <string>
+
+void ProfessionMobStrategy::PrintStrategyName() 
+{
+    File::LogFile("ProfessionMobStrategy", true);
+}
+
+int ProfessionMobStrategy::Execute() 
+{
+    StartFight();
+
+    while(!check::IsFightFinished()) {
+
+        WaitForMyTurn();
+
+        File::LogFile(("turn: " + std::to_string(_turn)).c_str(), true);
+
+        if(_turn % 4 == 0) {
+            ThrowSpellToMyself(SpellsCtrlRow::BASTION,   SpellsCtrlRow::SPELLS_CTRL_ROW);
+        } else if(_turn % 4 == 1) {
+            ThrowSpellToMyself(SpellsCtrlRow::MURALLA, SpellsCtrlRow::SPELLS_CTRL_ROW);
+            ThrowSpellToMyself(SpellsCtrlRow::ESCUDO, SpellsCtrlRow::SPELLS_CTRL_ROW);
+        } else {
+            ThrowSpellToMyself(SpellsCtrlRow::ATARAXIA, SpellsCtrlRow::SPELLS_CTRL_ROW);
+        }
+
+        ThrowSpellToEnemies(SpellsCtrlRow::WEAPON,  SpellsRow::SPELLS_ROW); // weapon is just 'q'
+        ThrowSpellToEnemies(SpellsRow::SILBO,       SpellsRow::SPELLS_ROW);
+        ThrowSpellToEnemies(SpellsRow::BORRASCA,    SpellsRow::SPELLS_ROW);
+        ThrowSpellToEnemies(SpellsRow::ESCALOFRIO,  SpellsRow::SPELLS_ROW);
+        ThrowSpellToEnemies(SpellsRow::MANIOBRA,    SpellsRow::SPELLS_ROW);
+        ThrowSpellToEnemies(SpellsRow::PASTO,       SpellsRow::SPELLS_ROW);
+        ThrowSpellToEnemies(SpellsRow::MOON,        SpellsRow::SPELLS_ROW);
+        ThrowSpellToEnemies(SpellsRow::LLAMILLA,    SpellsRow::SPELLS_ROW);
+
+        if(_enemiesXPositionInMenuFight.size() == 1) {
+            ThrowSpellToEnemies(SpellsCtrlRow::ESTRATO,   SpellsCtrlRow::SPELLS_CTRL_ROW);
+            ThrowSpellToEnemies(SpellsCtrlRow::ESCAPADITA, SpellsCtrlRow::SPELLS_CTRL_ROW);
+            ThrowSpellToEnemies(SpellsCtrlRow::ESCARCHA,   SpellsCtrlRow::SPELLS_CTRL_ROW);
+            ThrowSpellToEnemies(SpellsRow::NATURAL,        SpellsRow::SPELLS_ROW);
+        }
+
+        RandomMovePj(12); // Move randomly the Pj!
+
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+        if(!check::IsRecallPoti()) {
+            ThrowSpellToEnemies(SpellsCtrlRow::POMPA,      SpellsCtrlRow::SPELLS_CTRL_ROW);
+        }
+
+        PassTurn();
+
+        ++_turn;
+    }
+
+    return AfterFight();;
+}
